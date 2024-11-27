4546d4a790146a9acf1a5244f2d086b1ce4593e5
neinu <noms22@hotmail.com>
2024-11-27 15:07:47 +0200

    added the java script

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,22 @@
-alert("hello");
+function updateTime() {
+    // harare
+    let harareElement = document.querySelector("#harare");
+    let harareDateElement = harareElement.querySelector(".date");
+    let harareTimeElement = harareElement.querySelector(".time");
+    let harareTime = moment().tz("Africa/Harare");
+    harareDateElement.innerHTML = harareTime.format("MMMM Do YYYY");
+    harareTimeElement.innerHTML = harareTime.format("h:mm:ss [<small>]A[</small>]");
+
+    //bulawayo
+    let bulawayoElement = document.querySelector("#bulawayo");
+    let bulawayoDateElement = bulawayoElement.querySelector(".date");
+    let bulawayoTimeElement = bulawayoElement.querySelector(".time");
+    let bulawayoTime = moment().tz("Africa/Bulawayo");
+    bulawayoDateElement.innerHTML = bulawayoTime.format("MMMM Do YYYY");
+    bulawayoTimeElement.innerHTML = bulawayoTime.format("h:mm:ss [<small>]A[</small>]");
+
+
+}
+
+updateTime();
+setInterval(updateTime, 1000);
